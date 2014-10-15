a1bb20f373f0e40d867c3fcd3fd2501aa1e1d3a4
Tom Bran <tom.bran@gmail.com>
2014-10-15 15:52:38 +0100

    Fix: re-adding 'add contract' button (whoops); adding auto-complete functionality for contacts field. Not yet perfect

--- src/js/controllers/addController.js
+++ src/js/controllers/addController.js
@@ -1,14 +1,22 @@
 App.controller('AddController', [
     '$rootScope',
     '$scope',
+    '$timeout',
     'companiesService',
-    function($rootScope, $scope, companiesService) {
+    function($rootScope, $scope, $timeout, companiesService) {
         $rootScope.page = 'add';
         $scope.mode = 'new';
         $scope.isEditing = true;
 
         $scope.company = {};
         $scope.company.contracts = [];
+
+        $scope.$watch(
+            companiesService.getCompanies,
+
+            function(newVal, oldVal) {
+                $scope.contacts = companiesService.getContacts();
+            }, true);
 
         $scope.saveCompany = function() {
             companiesService.addCompany($scope.company);
@@ -22,6 +30,17 @@ App.controller('AddController', [
             if (confirm('Are you sure?')) {
                 $scope.company.contracts.splice($index, 1);
             }
+        };
+
+        $scope.selectContact = function(contact) {
+            $scope.company.contactName = contact;
+            $scope.showContacts = false;
+        }
+
+        $scope.delayBlur = function() {
+            $timeout(function() {
+                $scope.showContacts = false;
+            }, 250);
         };
     }
 ]);
