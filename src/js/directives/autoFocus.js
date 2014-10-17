c02664f4f58f5870e50cf5ba27b53d9547952fce
Tom Bran <tom.bran@gmail.com>
2014-10-17 11:27:25 +0100

    Adding auto-focus directive for form pages, to focus into first field on load

---
+++ src/js/directives/autoFocus.js
@@ -0,0 +1,10 @@
+App.directive('autoFocus', function($timeout) {
+    return {
+        restrict: 'AC',
+        link: function(_scope, _element) {
+            $timeout(function(){
+                _element[0].focus();
+            }, 0);
+        }
+    };
+});
